96373ba4bc6c2b95e578c6dc911b8d6847e85a8e
Chanpreet Singh <chanpreet.singh@sachtechsolution.com>
2019-10-10 09:33:59 +0530

    Initial Commit

---
+++ Sweat App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sweat App
+//
+//  Created by SachTech on 10/10/19.
+//  Copyright © 2019 SachTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
